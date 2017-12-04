4141129905f42d0a1c768530c1abb8dd38e2a1d6
Etherless <pabloimrik17@gmail.com>
2017-12-05 00:02:47 +0100

    Get characters reducer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ import React from 'react';
 import ReactDom from 'react-dom'
 import App from './components/App.js';
 import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer/index';
+import { getCharacters } from "./reducer/characters/actions";
+
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+store.dispatch(getCharacters());
 
 require('./index.html');
 
@@ -11,7 +23,9 @@ const container = document.getElementById('app-container');
 
 ReactDom.render(
     <AppContainer>
+        <Provider store={store}>
         <App/>
+        </Provider>
     </AppContainer>,
     container
 );
@@ -20,7 +34,9 @@ if( module.hot) {
     module.hot.accept('./components/App', () => {
         ReactDom.render(
             <AppContainer>
+                <Provider store={store}>
                 <App/>
+                </Provider>
             </AppContainer>,
             container
         );
