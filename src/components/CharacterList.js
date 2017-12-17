65c606ce7d6e06dd4e54d1b59a455f2201f3353f
Etherless <pabloimrik17@gmail.com>
2017-12-17 20:07:16 +0100

    Added setCharacter (active) function to Characterlist component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import { setCurrentCharacter } from "../reducer/character/actions";
 
-const CharacterList = ({ characters }) =>
+const CharacterList = ({ characters, setCharacter }) =>
     <div id="character-list" className="col-md-6">
         <h1>Characters</h1>
         <ul>
-            {characters.map(c =>
-                <li key={c.name}>
+            {characters.map((c, i) =>
+                <li key={c.name} onClick={setCharacter(i + 1)}>
                     {c.name}
                 </li>
             )}
@@ -18,4 +19,12 @@ const CharacterList = ({ characters }) =>
 
 const mapStateToProps = ({ characters }) => ({ characters });
 
-export default connect(mapStateToProps)(CharacterList);
+const mapDispatchToProps = dispatch => ({
+    setCharacter(id) {
+        return () => {
+            dispatch(setCurrentCharacter(id));
+        };
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
---
+++ src/reducer/character/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import id from './id';
+
+export default combineReducers({ id, });
